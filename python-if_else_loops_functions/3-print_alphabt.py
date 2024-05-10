a288b45f50d37c954ea1ff8f420019bdf60d8b89
gofastpanam <8972@holbertonstudents.com>
2024-05-10 16:56:55 +0200

    .

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(ord('a'), ord('z') + 1):
-    if format(i) != 'e' and format(i) != 'q':
-        print("{:c}".format(i), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
