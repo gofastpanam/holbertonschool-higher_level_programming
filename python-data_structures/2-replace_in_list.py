f5d20752a991491eb4f65160b41faa8e05825701
gofastpanam <8972@holbertonstudents.com>
2024-05-13 23:13:35 +0200

    .

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i, element in enumerate(my_list):
-            new_element = (idx * 3)
-            if idx == i:
-                my_list[idx] = new_element
-                return (my_list)
-            if idx < 0:
-                return (my_list)
-            elif idx > len(my_list):
-                return (my_list)
-
+    for i, num in enumerate(my_list):
+        if idx < 0 or idx > len(my_list):
+            return my_list
+        my_list[idx] = element
+        return my_list
