9018c944775069f1ca22bcae46495cbdad3e1237
gofastpanam <8972@holbertonstudents.com>
2024-05-15 01:55:18 +0200

    9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max = my_list[0]
+        for i in my_list[1:]:
+            if i > max:
+                max = i
+                return (max)
+            else:
+                pass
+    else:
+        return (None)
