1cc0a989cf9e88c84f6d1e728056c60650a1d1c7
gofastpanam <8972@holbertonstudents.com>
2024-05-16 23:09:13 +0200

    4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            division_result = 0
+            if i < len(my_list_1) and i < len(my_list_2):
+                if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                    if my_list_2[i] != 0:
+                        division_result = my_list_1[i] / my_list_2[i]
+                    else:
+                        print("division by 0")
+                else:
+                    print("wrong type")
+            else:
+                print("out of range")
+        except ZeroDivisionError:
+            print("division by 0")
+        finally:
+            result.append(division_result)
+    return result
