483f31d2808ba1690bc87bc41c75b5b4d60bcb6b
gofastpanam <8972@holbertonstudents.com>
2024-05-22 00:13:24 +0200

    .

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,13 +16,14 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    special_char = {".", "?", ":"}
+    special_char = [".", "?", ":"]
     i = 0
     while i < len(text):
         print(text[i], end='')
         if text[i] in special_char:
-            print("\n\n", end='')
+            print("\n")
             i += 1
             while i < len(text) and text[i] == ' ':
-                i -= 1
+                i += 1
+            continue
         i += 1
