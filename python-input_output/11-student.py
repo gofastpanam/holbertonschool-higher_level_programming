6544fd4ea96541ef64d0ae0b596d532a98b865f8
gofastpanam <8972@holbertonstudents.com>
2024-05-30 10:14:17 +0200

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module provides a class Student
+"""
+
+
+class Student:
+    """
+    The class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            if isinstance(attrs, list) and all(isinstance(attr, str)
+                                               for attr in attrs):
+                return {attr: getattr(self, attr)
+                        for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
