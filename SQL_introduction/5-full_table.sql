a4725d98bb5395abcb3b4d25d6bfe01631d7d540
gofastpanam <8972@holbertonstudents.com>
2024-06-18 13:51:20 +0200

    5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
--- Creates a full table with description 
-CREATE TABLE first_table (
-id INT DEFAULT NULL,
-name VARCHAR(256) DEFAULT NULL
-)
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Print a full table with description 
+SHOW CREATE TABLE first_table;
