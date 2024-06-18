f1e5ca23d1460e4b4f3d64be85f388974f7bee3a
gofastpanam <8972@holbertonstudents.com>
2024-06-18 17:11:58 +0200

    15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- Number by score
-SELECT COUNT(score) AS number FROM second_table WHERE score == score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table
+GROUP BY score ORDER BY number DESC;
