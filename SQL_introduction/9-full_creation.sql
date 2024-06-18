5be285cc1346e7d98b8d56abdeb5cffab570f91c
gofastpanam <8972@holbertonstudents.com>
2024-06-18 14:19:55 +0200

    9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- full_creation.sql
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+); 
+INSERT INTO (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO (id, name, score) VALUES (4, 'George', 8);
--- SQL_introduction/README.md
+++ SQL_introduction/README.md
@@ -9,3 +9,4 @@
 ## 6. List all in table
 ## 7. First add
 ## 8. Count 89
+## 9. Full creation
