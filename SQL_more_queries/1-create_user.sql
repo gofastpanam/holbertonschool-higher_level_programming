3f76e13ed0dc56a629a75e88cb89590f5c3a7bce
gofastpanam <8972@holbertonstudents.com>
2024-06-19 11:35:54 +0200

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the MySQL server user user_0d_1
 CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON MySQL TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON mysql TO 'user_0d_1'@'localhost';
