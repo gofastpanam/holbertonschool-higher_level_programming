ff4d9b49b40ad79b6d44e54e5dca8e1930917d34
gofastpanam <8972@holbertonstudents.com>
2024-06-21 16:28:22 +0200

    8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT cities WHERE name = California ORDER BY cities.id ASC;
+SELECT ALL cities WHERE name = California ORDER BY cities.id ASC;
