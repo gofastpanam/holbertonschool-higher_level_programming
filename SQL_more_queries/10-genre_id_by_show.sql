379eadac9824d304440173a4f976dd321eb7c529
gofastpanam <8972@holbertonstudents.com>
2024-06-21 17:44:07 +0200

    10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,4 @@
 
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-SELECT title, genre_id FROM tv_shows WHERE genre_id > 0 ORDER BY tv_shows.title;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE genre_id > 0 ORDER BY tv_shows.title;
