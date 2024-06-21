0bc1191d0e54f8637b58e8d47104403bccbb0941
gofastpanam <8972@holbertonstudents.com>
2024-06-21 16:36:49 +0200

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (state_id) INT NOT NULL REFERENCES states(id),
-    name VARCHAR(256) NOT NULL
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
