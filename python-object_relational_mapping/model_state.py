9a3b26294fead6613aac59ef579743e6812fcc7a
gofastpanam <8972@holbertonstudents.com>
2024-06-23 11:18:29 +0200

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,16 +4,34 @@ This module provides a class State links to the MYSQL table states
 """
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine(
-    'mysql+mycient://{}:{}@localhost:3306/hbtn_0e_6_usa',
-    echo=True)
+import sys
 
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class representing the states table in the database.
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128))
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Enter 'username', 'pass', 'database_name'.")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, database), pool_pre_ping=True)
+
+    # Create all tables in the database
+    Base.metadata.create_all(engine)
