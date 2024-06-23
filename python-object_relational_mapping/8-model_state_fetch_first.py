5a0729986b32e17288e3c28d54488c1bef644929
gofastpanam <8972@holbertonstudents.com>
2024-06-23 12:32:00 +0200

    8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,7 +5,7 @@ This module prints the first State object from the database hbtn_0e_6_usa
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    first_state = session.query(State).first()
+    first_state = session.query(State).order_by(State.id).first()
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
